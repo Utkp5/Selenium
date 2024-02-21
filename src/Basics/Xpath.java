43e5d8c1940a314018613b4df3618cab077d9ed1
utkarsh.pandit <utkp200@gmail.com>
2024-02-21 13:01:00 +0530

    Xpath

--- src/Basics/Xpath.java
+++ src/Basics/Xpath.java
@@ -1,11 +1,26 @@
 package Basics;
 
+import org.openqa.selenium.By;
+
 public class Xpath extends Utilizingmethods{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
+		// How to write our own xpath?
+		//*[@attribute='value']
+		//tagname[@attribute='value']
+		
+
 		
 		preforChromeCondition();
 		driver.get("https://demowebshop.tricentis.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//input[@name='NewsletterEmail']")).sendKeys("Ya sure I want subscription!"); 
+		System.out.println("success!");
+		
+		
 		
 	}
 	
